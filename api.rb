88dcc10950cd6c6351f6dcdd3a15c5f8b6d41443
Russell Clare <Russell@Clare.io>
2016-03-20 09:54:04 +0000

    Adding in files

---
+++ api.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+require 'sinatra'
+require 'open-uri'
+require 'json'
+
+def get_atlassian_release(app)
+  # Get payload from Atlassian
+  json = open("https://my.atlassian.com/download/feeds/current/#{app}.json") { |f| f.read }
+
+  # Parse it & Clean up as Atlassian use non standard JSON...
+  data = JSON.parse(json.dup.gsub("downloads(", "")[0...-1])
+
+  # Filter down to unix results
+  unix = data.select {|i| i['platform'] == 'Unix' }.select {|i| i['zipUrl'].include?('x64')}
+
+  # select the latest release number
+  latest = unix.max {|a,b| a['version'] <=> b['version'] }
+  return {
+    "version" => latest['version'],
+    "url" => latest['zipUrl'],
+    "released" => latest['released']
+  }
+
+end
+
+get '/:app' do
+  app = params['app']
+  get_atlassian_release(app).to_json
+end
